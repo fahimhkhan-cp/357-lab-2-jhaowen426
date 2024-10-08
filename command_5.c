ef00c53762b4002a5b96c3e51a0ec269df74e61a
jhaowen426 <jhaowen426@gmail.com>
2024-10-08 13:05:48 -0700

    Cleaned up command_5 code

--- command_5.c
+++ command_5.c
@@ -2,17 +2,13 @@
 #include <string.h>
 
 int main(int argc, char* argv[]) {
-    // initializes comparison condition "-"
-    char s1[2] = "-";
-    char* s2;
 
+    // iterates through each argument
     for (int i = 1; i < argc; i++) {
-        // take stdin one argument at a time
-        s2 = argv[i];
 
-        // print argument if argument begins with "-"
-        if (s2[0] == s1[0])  {
-            printf("%s\n", s2);
+        // print argument if argument begins with '-'
+        if (argv[i][0] == '-')  {
+            printf("%s\n", argv[i]);
         }
 
     }
