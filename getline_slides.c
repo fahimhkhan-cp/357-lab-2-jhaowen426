1df9c43fe1ceb3fc77055e2a6a049d0468da47ed
jhaowen426 <jhaowen426@gmail.com>
2024-10-10 16:48:08 -0700

    Fixed memory leaks

--- getline_slides.c
+++ getline_slides.c
@@ -1,5 +1,6 @@
 #define _GNU_SOURCE
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 int main(int argc, char* argv[]) {
@@ -20,33 +21,28 @@ int main(int argc, char* argv[]) {
         FILE *file = fopen(argv[1], "r");
 
         // keeps track of ultimate & penultimate lines
-        char *prev_line = "";
-        char *current_line = "";
+        char *last_line1 = NULL;
+        char *last_line2 = NULL;
 
         /*
         initializes variables needed for getline() function]
         (next_line) checks if end of file has been reached
         (getline()) returns -1 after reaching end of file
         */
-        char *next_line = "";
-        size_t next_n = 0;
-        int end = 0;
-
-        /*
-        keeps track of if there is 1 or more lines
-        (determines whether 1 or 2 lines are printed)
-        */
-        int lines = 0;
+        char *line = NULL;
+        unsigned int len = 0;
+        int read;
 
         // loops until reaching end of file
-        while (end != -1) {
+        while ((read = getline(&line, (size_t *)&len, file)) != -1) {
+
+            if (last_line2) {
+                free(last_line2);
+            }
 
             // only update strings if not at end of file yet
-            if ((end = getline(&next_line, &next_n, file)) != -1) {
-                prev_line = current_line;
-                current_line = next_line;
-                lines++;
-            }
+            last_line2 = last_line1;
+            last_line1 = strdup(line);
 
         }
 
@@ -54,13 +50,17 @@ int main(int argc, char* argv[]) {
         only print second line if there is > 1 line
         (always prints atleast 1 line)
         */
-        if (lines > 1) {
-            printf("%s", prev_line);
+        if (last_line2) {
+            printf("%s", last_line2);
         }
-        printf("%s\n", current_line);
+        if (last_line1) {
+            printf("%s", last_line1);
+        }
 
         // frees up used memory
-        free(next_line);
+        free(last_line1);
+        free(last_line2);
+        free(line);
         fclose(file);
         
     }
